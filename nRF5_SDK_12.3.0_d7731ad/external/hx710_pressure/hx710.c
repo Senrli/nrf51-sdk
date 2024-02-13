c667d666982a4b7fbf730f5914b92a4c2cbc1746
Senrli <19980321lsr@gmail.com>
2024-02-13 11:42:02 +0800

    feat: Added support for HX710 pressure sensor

---
+++ nRF5_SDK_12.3.0_d7731ad/external/hx710_pressure/hx710.c
@@ -0,0 +1,54 @@
+/**
+ * @file hx710.c
+ * @author your name (you@domain.com)
+ * @brief Driver source file for HX710B driver, adopted from the source datasheet. Compatible with HX710A.
+ * @version 0.1
+ * @date 2024-02-08
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "hx710.h"
+
+
+void hx710_init(hx710_t* sensor) {
+    nrf_gpio_cfg_output(sensor->sck);
+    nrf_gpio_cfg_input(sensor->dat, NRF_GPIO_PIN_PULLUP);
+}
+
+
+void get_weight_gross(hx710_t* sensor);
+
+unsigned int get_weight(hx710_t* sensor);
+
+unsigned long hx710_read(hx710_t* sensor) {
+    unsigned long count;
+    nrf_gpio_pin_write(sensor->sck, 1);
+    // nrf_delay_us(1);
+    nrf_gpio_pin_write(sensor->sck, 0);
+    // nrf_delay_us(1);
+    count = 0;
+    while (nrf_gpio_pin_read(sensor->dat));
+    for (int i = 0; i < HX710_ACCU; i++) {
+        // if(i >= HX710_BITS){
+        //     nrf_gpio_pin_write(sensor->dat, 1);
+        // }
+        nrf_gpio_pin_write(sensor->sck, 1);
+        // nrf_delay_us(1);
+        nrf_gpio_pin_write(sensor->sck, 0);
+        // nrf_delay_us(1);
+        if (i < HX710_BITS) {
+            count = count << 1;
+            if (nrf_gpio_pin_read(sensor->dat)) {
+                count++;
+            }
+        }
+    }
+    nrf_gpio_pin_write(sensor->sck, 1);
+    count ^= 0x800000;
+    // nrf_delay_us(1);
+    nrf_gpio_pin_write(sensor->sck, 0);
+    // nrf_delay_us(1);
+    return count;
+}
---
+++ nRF5_SDK_12.3.0_d7731ad/external/hx710_pressure/hx710.h
@@ -0,0 +1,47 @@
+/**
+ * @file hx710.h
+ * @author your name (you@domain.com)
+ * @brief Header file for HX710B driver.
+ * @version 0.1
+ * @date 2024-02-08
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#ifndef _HX710_H_
+#define _HX710_H_
+
+#include "nrf_gpio.h"
+#include "nrf_delay.h"
+
+#define HX710_10_HZ_TICKS 24
+#define HX710_40_HZ_TICKS 26
+#define HX710_BITS 24
+
+#define HX710_ACCU HX710_40_HZ_TICKS
+
+// volatile long hx710_buf = 0;
+// volatile long weight_gross = 0;
+// volatile long weight_actual = 0;
+
+typedef struct {
+    int sck; // sck pin for hx710 controller
+    int dat; // data pin for hx710 controller
+} hx710_t;
+
+/// @brief Initialize the HX710 sensor
+/// @param sensor sensor struct used
+void hx710_init(hx710_t * sensor);
+
+/// @brief Get gross weight 
+void get_weight_gross(hx710_t * sensor);
+
+/// @brief Get weight after calibration factor
+/// @return weight in calibrated values 
+unsigned int get_weight(hx710_t * sensor);
+
+/// @brief Read data from HX710 sensor
+/// @return Raw reading from HX710 sensor
+unsigned long hx710_read(hx710_t * sensor);
+
+#endif /* _HX710_H_ */
